e6dce37fabcf827ecc6ce4969272d1fb3b9cb42f
Ifanfomin <ifanfomin@rambler.ru>
2024-03-07 22:54:13 +0300

    Добавил автовоспроизведение поочереди

--- music/music.js
+++ music/music.js
@@ -1,3 +1,23 @@
+var music_deque = {
+    "I Loved Another Woman.mp3": "Only You.mp3",
+    "Only You.mp3": "Blood And Rockets.mp3",
+    "Blood And Rockets.mp3": "Сатана.mp3",
+    "Сатана.mp3": "Aria Math (Phonk).mp3",
+    "Aria Math (Phonk).mp3": "Robots Outro.mp3",
+    "Robots Outro.mp3": "I Loved Another Woman.mp3",
+};
+
+function next_track() {
+    var track = document.getElementById("pleer").src;
+    track = track.slice(track.lastIndexOf("/") + 1);
+    track = track.replaceAll("%20", " ");
+    alert(track);
+    var new_track = "audio/" + music_deque[track];
+    pleer.setAttribute("src", new_track);
+    audio.load();
+    audio.play();
+}
+
 function set_track(track_name) {
     var audio = document.getElementById("audio");
     var pleer = document.getElementById("pleer");
